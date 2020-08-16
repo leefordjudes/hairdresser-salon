32b4d270628bbefd9eb65751b39d6e41df5d7bcf
leefordjudes <leefordjudes@gmail.com>
2020-08-16 22:44:33 +0530

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAReD-OwWs8asOvDvpVSyQU3mg-FqzyQT0',
+    authDomain: 'my-hairdresser-salon.firebaseapp.com',
+    databaseURL: 'https://my-hairdresser-salon.firebaseio.com',
+    projectId: 'my-hairdresser-salon',
+    storageBucket: 'my-hairdresser-salon.appspot.com',
+    messagingSenderId: '954607430222',
+    appId: '1:954607430222:web:037b4f7c5bfcdf3f02711f'
+  },
 };
 
 /*
